bbdf41fa74a5a63846e4a0a194aaf825b2e105e0
Yaroslav Solokha <yaroslav.solokha@gmail.com>
2017-06-12 00:38:36 +0200

    Fix permissions

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -15,19 +15,23 @@ class DefaultController extends Controller
 {
   public function indexAction()
   {
+    $userId = $this->get('security.token_storage')->getToken()->getUser()->getId();
     $blogPosts = $this->getDoctrine()->getRepository('YSBlogBundle:BlogPost')->findAllOrderedByTitle();
     $categories = $this->getDoctrine()->getRepository('YSBlogBundle:Category')->findAll();
     return $this->render('YSBlogBundle:Default:index.html.twig', array (
       'blogPosts' => $blogPosts,
-      'categories' => $categories
+      'categories' => $categories,
+      'userId' => $userId
     ));
   }
 
   public function showAction(Request $request)
   {
+    $userId = $this->get('security.token_storage')->getToken()->getUser()->getId();
     $blogPost = $this->getDoctrine()->getRepository('YSBlogBundle:BlogPost')->find($request->get('id'));
     return $this->render('YSBlogBundle:Default:show.html.twig', array (
-      'blogPost' => $blogPost
+      'blogPost' => $blogPost,
+      'userId' => $userId
     ));
   }
 
@@ -44,8 +48,12 @@ class DefaultController extends Controller
 
   public function formAction(Request $request)
   {
+    $userId = $this->get('security.token_storage')->getToken()->getUser()->getId();
     if($request->get('id')) {
       $blogPost = $this->getDoctrine()->getRepository('YSBlogBundle:BlogPost')->find($request->get('id'));
+      if($userId != $blogPost->getUser()->getId()){
+        return $this->redirectToRoute('ys_blog_list');
+      }
     } else {
       $blogPost = new BlogPost($this->get('security.token_storage'));
     }
